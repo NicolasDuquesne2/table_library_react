370da1d250360abdfcae4d8dde9ff9b40731b964
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-08-17 17:01:16 +0200

    useTableData doc written

--- src/Hooks/useTableData.jsx
+++ src/Hooks/useTableData.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react"
 
+
+/**
+ * 
+ * @module useTableData
+ */
+
+/**
+ * This hook returns parms usefull for the pagination component
+ *  
+ * @return {Object} tableData
+ * @param {number} data row datas before manipulations
+ * @param {number} rowsPerPage rows per page given by the pagination selector
+ * @param {number} pageNumber page number selected
+ * 
+ */
 export const useTableData = (data, rowsPerPage, pageNumber) => {
     const [oiginalData] = useState(data)
     const [tableData , setTableData] = useState(data)
 
+    /**
+     * this function filters the datas according to string received
+     * setTableData with a new datas set
+     * @param {string} filter 
+     */
     const filter = (filter) => {
         
         let filtered = null
@@ -18,6 +38,12 @@ export const useTableData = (data, rowsPerPage, pageNumber) => {
         setTableData(filtered)
       }
 
+    /**
+     * this function sorts the datas according to field and an order get
+     * setTableData with a new sorted datas set
+     * @param {string} sortField the field wich sorts datas 
+     * @param {string} sortOrder the order is ascending or descending
+     */
     const sorting = (sortField, sortOrder) => {
       if (sortField) {
         const sorted = [...tableData].sort((a, b) => {
@@ -34,6 +60,10 @@ export const useTableData = (data, rowsPerPage, pageNumber) => {
       }
     }
 
+    /**
+     * this function slices the datas according to a min index and max index, depending on page number and rows per page
+     * @return {Array}
+     */
     const sliceTableData = () => {
 
       const indexOfLastRow = pageNumber * rowsPerPage
@@ -42,6 +72,10 @@ export const useTableData = (data, rowsPerPage, pageNumber) => {
       return [...tableData.slice(indexOfFirstRow, indexOfLastRow)]
     }
 
+    /**
+     * this function returns the current datas length
+     * @return {number}
+     */
     const getTableLength = () => {
       return tableData.length
     }
