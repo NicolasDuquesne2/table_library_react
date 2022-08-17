d1faf2864bc4974579d44070fa100b36a9467fe0
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-08-17 14:05:39 +0200

    select menu component is now documented

--- src/Components/SelectMenu/index.jsx
+++ src/Components/SelectMenu/index.jsx
@@ -9,14 +9,35 @@ import './selectmenu.css'
 
 
 /**
- * Renders a list of select menu options
+ * Renders a list of select menu options from an array or an object
+ * @param {Object} params contains keyPart, datas, typOfDatas, dataAttr
  * @returns {React.ReactComponentElement}
  */
 function SelectMenu({params}) {
 
+    /***
+     * optionKind is prefix for options keys
+     * @type {String}
+     * @alias module:SelectMenu.optionKind
+     */
     const optionKind = params.keyPart
+    /***
+     * datas contains all list values for the select menu
+     * @type {Array|Object}
+     * @alias module:SelectMenu.datas
+     */
     const datas = params.datas
+    /***
+     * typeOfDatas indicates the nature of the value container. an array or an object
+     * @type {string}
+     * @alias module:SelectMenu.typeOfDatas
+     */
     const typeOfDatas = params.typeOfDatas
+    /***
+     * dataAttribute is an optional argument involved by objects. Indicates wich objecte attribute containes the value
+     * @type {string}
+     * @alias module:SelectMenu.dataAttribute
+     */
     const dataAttribute = params.dataAttr
     
 
