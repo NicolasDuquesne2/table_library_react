f1f43e228fe9952ab23466c4c936bf51fa8d56b3
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-08-22 18:02:50 +0200

    put elements to publish in a lib directory. Installed babel dépendances

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import React from "react"
 import {createRoot} from 'react-dom/client'
-import Table from './Components/Table'
-import { dataMoke, columns} from './params/dataMoke'
+import Table from './lib/Components/Table'
 
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-    <Table payload={{datas: dataMoke, columns}} />
+    <Table payload={{datas: null, columns:null}} />
 )
